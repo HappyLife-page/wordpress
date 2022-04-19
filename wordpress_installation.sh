43eb54db18c5f52a4637cd6c66245c92cc8cbf42
HappyLife-page <84018863+HappyLife-page@users.noreply.github.com>
2022-04-20 07:24:40 +0800

    Update wordpress_installation.sh
    添加wordpress的nginx访问日志

--- wordpress_installation.sh
+++ wordpress_installation.sh
@@ -103,6 +103,9 @@ server {
         ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
 	ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
 
+        access_log /var/log/nginx/$wp_domainName.access.log;
+        error_log /var/log/nginx/$wp_domainName.error.log;
+
         root ${wp_code_dir}/wordpress;
         index index.php;
 
